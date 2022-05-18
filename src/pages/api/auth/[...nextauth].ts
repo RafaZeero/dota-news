8ab82a749cb4029d77286413d74c8e8fddb28aac
Rafael Lima | Zeero <rafaa99@gmail.com>
2022-05-18 19:41:53 -0300

    Final deploy

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,7 +2,6 @@ import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 import { fauna } from '../../../services/fauna'
 import { query as q } from 'faunadb'
-
 export default NextAuth({
   secret: process.env.NEXTAUTH_SECRET,
   // Configure one or more authentication providers
--- src/services/fauna.ts
+++ src/services/fauna.ts
@@ -2,7 +2,5 @@ import { Client } from 'faunadb'
 
 //acesso ao bando de dados
 export const fauna = new Client({
-  secret: process.env.FAUNADB_KEY,
-  domain: 'db.us.fauna.com',
-  scheme: 'https'
+  secret: process.env.FAUNADB_KEY
 })
